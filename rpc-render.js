96516c018571153198e047498cb40c09caf33f1b
Tuomas Starck <dev@starck.fi>
2014-10-02 12:07:42 +0300

    2014-10-02-01

--- rpc-render.js
+++ rpc-render.js
@@ -1,23 +1,23 @@
-#!/usr/bin/phantomjs
+#!/usr/bin/env nodejs
 
 /*\
  *  rpc-render.js
  *  Media creation and conversion as a service.
- *  2014-09-29 / Meetin.gs
+ *  2014-10-02 / Meetin.gs
 \*/
 
-var app     = require('express')()
-var util    = require('util')
-var phantom = require('./lib/phantom.js')
-var pkg     = require('./package.json')
+var app  = require('express')()
+var util = require('util')
+var web  = require('./lib/web.js')
+var pkg  = require('./package.json')
 
 var port = parseInt(process.env.PORT, 10)
 
 /* FIXME Legacy support */
-app.get('/', phantom.capture)
+app.get('/', web.capture)
 
-app.get('/webcap', phantom.capture)
-app.post('/webcap', phantom.capture)
+app.get('/webcap', web.capture)
+app.post('/webcap', web.capture)
 
 app.get('/status', function(request, result) {
   result.status(200).send("status: running")
